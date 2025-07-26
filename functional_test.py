3a933415934c7cc1a10e180a3cc0113aaed67fc0
machao0x4B <machao0x4B@163.com>
2025-07-26 23:33:52 +0800

    Introduce unittest to write the first functional test case.

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,22 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goesto check out its homepage
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
