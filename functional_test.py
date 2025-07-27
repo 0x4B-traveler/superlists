303a398afac69f7eca9f7b1e5e6b8fd394464bfd
machao0x4B <machao0x4B@163.com>
2025-07-27 21:22:16 +0800

    Refactor functional test: retrieve a auxiliary functions.

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # She goesto check out its homepage
@@ -28,20 +33,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element(by=By.ID, value='id_list_table')
-        # returns a list of elements
-        rows = table.find_elements(by=By.TAG_NAME, value='tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element(by=By.ID, value='id_list_table')
-        # returns a list of elements
-        rows = table.find_elements(by=By.TAG_NAME, value='tr')
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
