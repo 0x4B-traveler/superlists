57b9a68680e9b9dd6340c2073e4f5838da933315
machao0x4B <machao0x4B@163.com>
2025-07-27 20:11:41 +0800

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -18,22 +18,24 @@ class NewVisitorTest(unittest.TestCase):
         # She goesto check out its homepage
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
+        header_text = self.browser.find_element(
+            by=By.TAG_NAME, value='h1').text
         self.assertIn('To-Do', header_text)
         inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
         self.assertEqual(inputbox.getattr('placeholder'), 'Enter a to-do item')
-        
+
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        
+
         time.sleep(1)
-        
+
         table = self.browser.find_element(by=By.ID, value='id_list_table')
-        rows = table.find_elements(by=By.TAG_NAME, value='tr') # returns a list of elements
+        # returns a list of elements
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
-        
+
         self.fail('Finish the test!')
 
 
